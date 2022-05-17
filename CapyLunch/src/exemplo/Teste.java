086bb9024be846bde6b0461fc177bf074d6804ce
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-05-17 12:31:37 -0300

    Criei um arquivo para testes

---
+++ CapyLunch/src/exemplo/Teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplo;
+
+/**
+ *
+ * @author kauas
+ */
+public class Teste {
+    
+    public static void main(String[] args) {
+        System.out.println("Capybara");
+    }
+}
