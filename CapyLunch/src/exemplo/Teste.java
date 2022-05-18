52ff2384bfb0578998d7aa1264038eb8f5910d2f
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-05-17 21:26:29 -0300

    Criei um arquivo para testes (5)

--- CapyLunch/src/exemplo/Teste.java
+++ CapyLunch/src/exemplo/Teste.java
@@ -11,9 +11,11 @@ package exemplo;
 public class Teste {
     
     public static void main(String[] args) {
+        String animal = "Dromedário";
         System.out.println("Capybara");
         System.out.println("Maria linda");
         System.out.println("A mensagem de cima é verdade!");
         System.out.println("Pedro sabe de tudo");
+        System.out.println(animal);
     }
 }
